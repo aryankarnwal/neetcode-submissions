c40d6bc89e388892e178324541e67d5693ab865c
Aryan Karnwal <aryankarnwal16@gmail.com>
2026-07-18 19:23:48 -0700

    Add: strategy - submission-0

---
+++ Design Patterns/strategy/submission-0.py
@@ -0,0 +1,56 @@
+class Person:
+    def __init__(self, lastName: str, age: int, married: bool):
+        self.lastName = lastName
+        self.age = age
+        self.married = married
+
+    def getLastName(self) -> str:
+        return self.lastName
+
+    def getAge(self) -> int:
+        return self.age
+
+    def isMarried(self) -> bool:
+        return self.married
+
+class PersonFilter(Protocol):
+    def apply(self, person: Person) -> bool:
+        ...
+
+class AdultFilter(PersonFilter):
+    def apply(self, person):
+        if person.age >= 18:
+            return True
+        
+        return False
+
+class SeniorFilter(PersonFilter):
+    def apply(self, person):
+        if person.age >= 65:
+            return True
+        
+        return False
+
+class MarriedFilter(PersonFilter):
+    def apply(self, person):
+        if person.married:
+            return True
+        
+        return False
+
+class PeopleCounter:
+    def __init__(self):
+        self.filter: PersonFilter = None
+
+    def setFilter(self, filter: PersonFilter) -> None:
+        self.filter = filter
+
+    def count(self, people: List[Person]) -> int:
+        res = 0
+
+        for p in people:
+            if self.filter.apply(p):
+                res += 1
+        
+        return res
+    
